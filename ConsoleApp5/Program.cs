509f7de0b46301e07560421995a7621d0f595a50
Nadir <wint1841@inbox.ru>
2023-12-18 14:41:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Лабораторная Работа №4
+            Console.Write("Введите X=");
+            double x = double.Parse(Console.ReadLine());
+
+            int power = 10;
+            double y = 0;
+
+            for (int k = 1; k <= 11; k++)
+            {
+                y += k * Pow(x, power);
+                power--;
+            }
+            Console.WriteLine($"Ответ: y = {y}");
+        }
+    }
+}
